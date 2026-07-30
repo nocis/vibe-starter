245a9864940d4243c86de4652a21fcd02fb37970
nocis <nocis@tju.edu.cn>
2026-07-31 07:43:52 +0800

    add default plugin setup

--- .vibe/.opencode/entrypoint.sh
+++ .vibe/.opencode/entrypoint.sh
@@ -3,6 +3,7 @@
 # need to create config file via this instead of mannually to avoid opencode inside container cannot write
 # ln -sf ~/.opencode/.opencode.json ~/.opencode.json
 ln -sf /app/.vibe/.opencode/opencode-config-local /app/.opencode
+ln -sf /app/.vibe/.opencode/config/.opencode/* /app/.opencode/
 
 # Create the link dynamically at runtime
 # -s = symbolic, -f = force (overwrite if exists)
